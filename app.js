62751d76cb4d86897cd07d2a7f67a635b4c1c749
Denis <122015463+iziKomSD@users.noreply.github.com>
2024-07-06 17:23:34 +0300

    timers in Node js

---
+++ app.js
@@ -0,0 +1,16 @@
+// const start = performance.now();
+
+// setTimeout(() => {
+//   console.log(performance.now() - start);
+//   console.log("Прошла секунда");
+// }, 1000);
+
+// function myFn(arg) {
+//   console.log(`Аргумент => ${arg}`);
+// }
+
+// setTimeout(myFn, 1500, "SUPER");
+
+setTimeout(() => {
+  console.log("BOOOOOM");
+}, 5000);
