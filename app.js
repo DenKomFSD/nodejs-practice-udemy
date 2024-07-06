503ee985d32b02cbba1b71a209a19dae9fbd7e4e
Denis <122015463+iziKomSD@users.noreply.github.com>
2024-07-06 22:51:36 +0300

    timers in Node js

--- app.js
+++ app.js
@@ -11,6 +11,43 @@
 
 // setTimeout(myFn, 1500, "SUPER");
 
+// const boom = setTimeout(() => {
+//   console.log("Bomb have been planted");
+// }, 5000);
+
+// setTimeout(() => {
+//   clearTimeout(boom);
+//   console.log("The bomb have been defused)");
+// }, 1000);
+
+//проверка на окончание
+
+const fs = require("fs");
+
+console.log("Init");
+
 setTimeout(() => {
-  console.log("BOOOOOM");
-}, 5000);
+  console.log(performance.now(), "Timer 100 sec.");
+}, 100);
+
+setImmediate(() => {
+  console.log("Timer immediate");
+  Promise.resolve().then(() => {
+    console.log("Promise 234");
+  });
+});
+
+fs.readFile(__filename, () => {
+  console.log("File read");
+  Promise.resolve().then(() => {
+    console.log("Promise 123");
+  });
+});
+
+Promise.resolve().then(() => {
+  console.log("Promise");
+});
+
+process.nextTick(() => console.log("Tick"));
+
+console.log("Final");
